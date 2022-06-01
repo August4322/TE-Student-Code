23aa03fe25b700ecf0b14e8320baca05f7e4d44e
Abigail Newman <newmanabigail@gmail.com>
2022-05-31 20:36:36 -0400

    Did tutorial for week 3 lesson 11

--- module-1/11_Inheritance/tutorial/src/main/java/com/techelevator/Bookstore.java
+++ module-1/11_Inheritance/tutorial/src/main/java/com/techelevator/Bookstore.java
@@ -23,6 +23,15 @@ public class Bookstore {
         shoppingCart.add(twoCities);
         shoppingCart.add(threeMusketeers);
         shoppingCart.add(childhoodEnd);
+
+        // Add some new movies and purchase them
+        Movie toyStory = new Movie("Toy Story", "G", 81, 19.99);
+        shoppingCart.add(toyStory);
+
+        // Shirley, you can't be serious!
+        Movie airplane = new Movie("Airplane!", "PG", 88, 14.99);
+        shoppingCart.add(airplane);
+
         System.out.println(shoppingCart.receipt());
     }
 }
