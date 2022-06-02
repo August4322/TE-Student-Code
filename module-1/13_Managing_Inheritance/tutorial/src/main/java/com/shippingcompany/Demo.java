87d551917edc656dc47a26c035eece96e5d20d90
Abigail Newman <newmanabigail@gmail.com>
2022-06-01 20:50:18 -0400

    Completed tutorials for week 3 Polymorphism and Managing Inheritance

--- module-1/13_Managing_Inheritance/tutorial/src/main/java/com/shippingcompany/Demo.java
+++ module-1/13_Managing_Inheritance/tutorial/src/main/java/com/shippingcompany/Demo.java
@@ -1,9 +1,17 @@
 package com.shippingcompany;
 
 
+import java.time.LocalDateTime;
+
 public class Demo {
 
     public static void main(String[] args) {
 
+        TruckDelivery myDelivery = new TruckDelivery();
+
+
+
     }
+
 }
+
