7421abaedc4326129e436c610f1b63594a644fb7
Abigail Newman <newmanabigail@gmail.com>
2022-06-07 09:52:56 -0400

    working through File to Reading Quiz challenge

--- module-1/16b_File_IO_Reading/exercise/src/main/java/com/techelevator/QuizMaker.java
+++ module-1/16b_File_IO_Reading/exercise/src/main/java/com/techelevator/QuizMaker.java
@@ -1,9 +1,36 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class QuizMaker {
 
 	public static void main(String[] args) {
-		
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Please input a viable file path for the quiz:");
+		String fileInput = input.nextLine();
+		File quiz = new File(fileInput);
+
+		int quizQuestionLine = 1;
+
+		try (Scanner quizReader = new Scanner(quiz)) {
+			while (quizReader.hasNextLine()) {
+				String currentLine = quizReader.nextLine();
+
+
+				System.out.println(currentLine.substring(0, currentLine.indexOf("|")));
+				System.out.println(currentLine.substring(currentLine.indexOf("|")));
+
+			}
+		} catch (FileNotFoundException ex) {
+			System.out.println("File was not found, check the path");
+		} catch(Exception ex) {
+			System.out.println("Something went wrong");
+		}
 
 	}
 
