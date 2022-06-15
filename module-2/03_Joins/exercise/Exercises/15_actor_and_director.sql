05474340de5d5effaad9b732f71a105fc1884814
Abigail Newman <newmanabigail@gmail.com>
2022-06-15 16:08:58 -0400

    complete week 5 Joins exercise

--- module-2/03_Joins/exercise/Exercises/15_actor_and_director.sql
+++ module-2/03_Joins/exercise/Exercises/15_actor_and_director.sql
@@ -1,2 +1,6 @@
 -- 15. The title of the movie and the name of director for movies where the director was also an actor in the same movie (73 rows)
-
+SELECT movie.title, person.person_name
+FROM person
+JOIN movie_actor ON person.person_id = movie_actor.actor_id
+JOIN movie ON movie_actor.movie_id = movie.movie_id
+WHERE movie_actor.actor_id = movie.director_id;
