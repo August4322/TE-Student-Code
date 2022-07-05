ff1c02a6d88047fdbe28443744c2a163977513c6
Abigail Newman <newmanabigail@gmail.com>
2022-07-05 14:26:42 -0400

    completed week 8 Securing APIs exercise

--- module-2/16_Securing_APIs/exercise/server/src/main/java/com/techelevator/auctions/controller/AuctionController.java
+++ module-2/16_Securing_APIs/exercise/server/src/main/java/com/techelevator/auctions/controller/AuctionController.java
@@ -4,13 +4,17 @@ import com.techelevator.auctions.dao.AuctionDao;
 import com.techelevator.auctions.dao.MemoryAuctionDao;
 import com.techelevator.auctions.exception.AuctionNotFoundException;
 import com.techelevator.auctions.model.Auction;
+import org.apache.tomcat.jni.User;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.nio.file.attribute.UserPrincipal;
 import java.util.List;
 
 @RestController
+@PreAuthorize("isAuthenticated()")
 @RequestMapping("/auctions")
 public class AuctionController {
 
@@ -20,6 +24,7 @@ public class AuctionController {
         this.dao = new MemoryAuctionDao();
     }
 
+    @PreAuthorize("permitAll")
     @RequestMapping(path = "", method = RequestMethod.GET)
     public List<Auction> list(@RequestParam(defaultValue = "") String title_like, @RequestParam(defaultValue = "0") double currentBid_lte) {
 
@@ -39,17 +44,20 @@ public class AuctionController {
     }
 
     @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("hasRole('ADMIN')" + "|| hasRole('CREATOR')")
     @RequestMapping(path = "", method = RequestMethod.POST)
     public Auction create(@Valid @RequestBody Auction auction) {
         return dao.create(auction);
     }
 
+    @PreAuthorize("hasRole('ADMIN')" + "|| hasRole('CREATOR')")
     @RequestMapping(path = "/{id}", method = RequestMethod.PUT)
     public Auction update(@Valid @RequestBody Auction auction, @PathVariable int id) throws AuctionNotFoundException {
         return dao.update(auction, id);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PreAuthorize("hasRole('ADMIN')")
     @RequestMapping(path = "/{id}", method = RequestMethod.DELETE)
     public void delete(@PathVariable int id) throws AuctionNotFoundException {
         dao.delete(id);
@@ -57,7 +65,7 @@ public class AuctionController {
 
     @RequestMapping(path = "/whoami")
     public String whoAmI() {
-        return "";
+        return "user";
     }
 
 }
