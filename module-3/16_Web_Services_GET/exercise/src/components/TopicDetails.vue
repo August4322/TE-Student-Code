65fa4b02b6e11c7b287145d4bac4f06ebf2e9c18
Abigail Newman <newmanabigail@gmail.com>
2022-08-03 21:33:07 -0400

    completed Week 12 Web Service Get exercise

--- module-3/16_Web_Services_GET/exercise/src/components/TopicDetails.vue
+++ module-3/16_Web_Services_GET/exercise/src/components/TopicDetails.vue
@@ -11,6 +11,7 @@
 </template>
 
 <script>
+import TopicService from '@/services/TopicService'
 export default {
   name: 'topic-details',
   props: {
@@ -24,6 +25,12 @@ export default {
         messages: []
       },
     }
+  },
+  created() {
+    TopicService.getTopicById(this.$route.params.id).then(
+      (x) => {
+        this.topic = x.data
+      });
   }
 }
 </script>
